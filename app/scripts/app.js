f095f142f62715e47d361cda8639e0db4868b36d
Jake Renzella <j@kerenzella.com>
2016-05-24 22:01:35 +1000

    FEATURE: Prepare for expandable card

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -74,6 +74,10 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
     app.$.headerPanelMain.scrollToTop(true);
   };
 
+  app.toggle = function() {
+  this.$.collapse.toggle();
+  };
+
   app.closeDrawer = function() {
     app.$.paperDrawerPanel.closeDrawer();
   };
